cc8235299453784ccdc77496ae66f0939f17fb11
eijin <eijin@EIJINAKASHIMA>
2025-03-19 14:20:52 +0900

    Add project files.

---
+++ DemoBlazorSSRUrlShortner/Data/AppDbcontext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoBlazorSSRUrlShortner.Data
+{
+    public class AppDbcontext(DbContextOptions<AppDbcontext> options) : DbContext (options)
+    {
+        public DbSet<UrlMapping> UrlMapping => Set<UrlMapping>();
+    }
+}
